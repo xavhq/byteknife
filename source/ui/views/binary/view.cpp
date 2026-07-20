0bbace43494222fccaf2da4d6d036133d87e18e5
xavhq <gifjaf@gmail.com>
2026-07-20 22:12:01 +0100

    tiny update: 0.1.2

--- source/ui/views/binary/view.cpp
+++ source/ui/views/binary/view.cpp
@@ -3,26 +3,6 @@
 
 namespace ui::views {
     BinaryView::BinaryView(QWidget* parent) : QWidget(parent) {
-        // Create a container widget for everything
-        QWidget* container = new QWidget(this);
-        QVBoxLayout* container_layout = new QVBoxLayout(container);
-        container_layout->setContentsMargins(0, 0, 0, 0);
-        container_layout->setSpacing(0);
-
-        // Create the message label
-        QLabel* message_label = new QLabel(this);
-        message_label->setText("No binary is loaded yet; Press <b>Ctrl+O</b> to open a binary");
-        message_label->setAlignment(Qt::AlignCenter);
-        message_label->setStyleSheet("QLabel {"
-                                     "    font-size: 14px;"
-                                     "    color: #666;"
-                                     "    padding: 20px;"
-                                     "    background-color: #f5f5f5;"
-                                     "    border-bottom: 1px solid #ddd;"
-                                     "}");
-        message_label->setVisible(false); // Initially hidden, show when no binary is loaded
-
-        // Create the splitter
         this->main_splitter_ = new QSplitter(Qt::Horizontal, this);
 
         this->functions_ = new QWidget(this);
@@ -44,13 +24,8 @@ namespace ui::views {
         this->main_splitter_->setStretchFactor(2, 1);
         this->main_splitter_->setChildrenCollapsible(false);
 
-        // Add everything to the container layout
-        container_layout->addWidget(message_label);
-        container_layout->addWidget(this->main_splitter_);
-
-        // Set the container as the main layout
         auto* layout = new QVBoxLayout(this);
         layout->setContentsMargins(0, 0, 0, 0);
-        layout->addWidget(container);
+        layout->addWidget(this->main_splitter_);
     }
 }
